35cb9f0af35f646440a447d6c3edb0260a45ad24
rdias66 <dias.a@live.com>
2024-11-05 11:04:40 -0300

    scripts finished

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
+
+# Update existing packages
 echo "Updating existing packages"
 sudo apt update
 sudo apt upgrade -y
 
+# Add base user
 echo "Adding base user"
-sudo adduser admin
+sudo adduser --gecos "" admin
 
-
+# Add base user to sudo
 echo "Adding base user to sudo"
 sudo usermod -a -G sudo admin
 
-
+# Install base packages
 echo "Installing base packages"
 sudo apt install -y htop git curl unzip jq
 
+# Install Docker
 echo "Installing Docker"
 sudo apt install -y \
     ca-certificates \
@@ -25,20 +29,39 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
-
-echo "Adding base users to Docker group"
+# Add base user to Docker group
+echo "Adding base user to Docker group"
 sudo usermod -a -G docker admin
 
-echo "Creating docker networks"
+# Create Docker networks
+echo "Creating Docker networks"
 sudo docker network create backend
 sudo docker network create frontend
 
+# Setup cron jobs for cleanup scripts
+echo "Setting up cron jobs for cleanup scripts"
+CRON_FILE=$(mktemp)
+
+# Update the file paths to match the admin user's folder structure
+echo "0 0 * * 6 /home/admin/aws-deploy-assist-infra/clean-docker.sh >> /home/admin/aws-deploy-assist-infra/cleaner-logs/clean-docker.log 2>&1" >> $CRON_FILE
+echo "0 0 * * 6 /home/admin/aws-deploy-assist-infra/clean-ecr.sh >> /home/admin/aws-deploy-assist-infra/cleaner-logs/clean-ecr.log 2>&1" >> $CRON_FILE
+
+# Install the new crontab from the temporary file
+sudo crontab $CRON_FILE
+
+# Remove the temporary file
+rm $CRON_FILE
+
+echo "Cron jobs have been set up successfully."
+
+# Install AWS CLI
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
 
+# Configure AWS credentials
 echo "Configuring AWS credentials"
 aws configure
