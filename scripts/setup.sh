3a27f40e61f252a7fc4a91b0eceff1be6870a95a
rdias66 <dias.a@live.com>
2024-12-17 14:12:53 -0300

    translate setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-# Update existing packages
-echo "Updating existing packages"
+# Atualizar pacotes existentes
+echo "Atualizando pacotes existentes"
 sudo apt update
 sudo apt upgrade -y
 
-# Add base user
-echo "Adding base user"
+# Adicionar usuário base
+echo "Adicionando usuário base"
 sudo adduser --gecos "" admin
 
-# Add base user to sudo
-echo "Adding base user to sudo"
+# Adicionar usuário base ao grupo sudo
+echo "Adicionando usuário base ao grupo sudo"
 sudo usermod -a -G sudo admin
 
-# Install base packages
-echo "Installing base packages"
+# Instalar pacotes base
+echo "Instalando pacotes base"
 sudo apt install -y htop git curl unzip jq
 
-# Install Docker
-echo "Installing Docker"
+# Instalar Docker
+echo "Instalando Docker"
 sudo apt install -y \
     ca-certificates \
     curl \
@@ -31,40 +31,40 @@ echo \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
-# Add base user to Docker group
-echo "Adding base user to Docker group"
+# Adicionar usuário base ao grupo Docker
+echo "Adicionando usuário base ao grupo Docker"
 sudo usermod -a -G docker admin
 
-# Create Docker networks
-echo "Creating Docker networks"
+# Criar redes Docker
+echo "Criando redes Docker"
 sudo docker network create backend
 sudo docker network create frontend
 
-# Setup cron jobs for cleanup scripts
-echo "Setting up cron jobs for cleanup scripts"
+# Configurar cron jobs para scripts de limpeza
+echo "Configurando cron jobs para scripts de limpeza"
 CRON_FILE=$(mktemp)
 
-# Update the file paths to match the admin user's folder structure
+# Atualizar os caminhos dos arquivos para corresponder à estrutura de pastas do usuário admin
 echo "0 3 * * * sudo /home/admin/aws-deploy-assist-infra/clean-docker.sh " >> $CRON_FILE
 echo "0 4 * * * sudo -E /home/admin/aws-deploy-assist-infra/clean-ecr.sh"  >> $CRON_FILE
 
-# Install the new crontab from the temporary file
+# Instalar o novo crontab a partir do arquivo temporário
 sudo crontab $CRON_FILE
 
-# Remove the temporary file
+# Remover o arquivo temporário
 rm $CRON_FILE
 
-echo "Cron jobs have been set up successfully."
+echo "Os cron jobs foram configurados com sucesso."
 
 sudo systemctl daemon-reload
 sudo systemctl restart cron
 
-# Install AWS CLI
-echo "Installing AWS CLI"
+# Instalar AWS CLI
+echo "Instalando AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
 
-# Configure AWS credentials
-echo "Configuring AWS credentials"
+# Configurar credenciais da AWS
+echo "Configurando credenciais da AWS"
 aws configure
