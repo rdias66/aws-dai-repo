a8479328dd4bd8a6ce9209c76d31331fb7e7397e
rdias66 <dias.a@live.com>
2024-12-17 14:39:51 -0300

    translate clean ecr

--- scripts/clean-ecr.sh
+++ scripts/clean-ecr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGION="sa-east-1"
-REPOSITORIES=("repo_name1" "repo_name2") # Change to the names of your repos(or repo) created in the tofu production module(the name on tfvars)
+REPOSITORIES=("repo_name1" "repo_name2") # Mude para os nomes dos repositórios criados no seu modulo de production
 
 LOG_DIR="./cleaner-logs"
 LOG_FILE="$LOG_DIR/clean-ecr-$(date +'%Y-%m-%d_%H-%M-%S').log"
@@ -13,7 +13,7 @@ log() {
 }
 
 for REPO_NAME in "${REPOSITORIES[@]}"; do
-  log "Cleaning up container repository: $REPO_NAME"
+  log "Limpando o repositório: $REPO_NAME"
 
   IMAGES_TO_DELETE=$(aws ecr list-images \
     --region $REGION \
@@ -21,12 +21,12 @@ for REPO_NAME in "${REPOSITORIES[@]}"; do
     --query 'imageIds[?(@.imageTag != `latest`)]' \
     --output json | jq -r '.[] | @base64')
 
-  # Check if any images were found
+
   if [ -z "$IMAGES_TO_DELETE" ]; then
-    log "No images to delete in repository: $REPO_NAME (only 'latest' tag found or no images at all)"
+    log "Nenhuma imagem para remover no repositório: $REPO_NAME (apenas tag 'latest' ou nenhuma imagem)"
     continue
   else
-    log "Images found for deletion in $REPO_NAME:"
+    log "Imagens encontradas para remoção no repositório $REPO_NAME:"
   fi
 
   # Delete images one by one
@@ -34,27 +34,27 @@ for REPO_NAME in "${REPOSITORIES[@]}"; do
     IMAGE_DIGEST=$(echo "$image" | base64 --decode | jq -r '.imageDigest')
     IMAGE_TAG=$(echo "$image" | base64 --decode | jq -r '.imageTag')
 
-    log "Found image with tag ${IMAGE_TAG:-null} (digest: $IMAGE_DIGEST) in repository $REPO_NAME"
+    log "Imagem encontrada com a ${IMAGE_TAG:-null} (digest: $IMAGE_DIGEST) no repositorio $REPO_NAME"
 
     if [ -n "$IMAGE_DIGEST" ] && [ "$IMAGE_TAG" != "latest" ]; then
-      log "Deleting image with tag ${IMAGE_TAG:-null} (digest: $IMAGE_DIGEST) in repository $REPO_NAME..."
+      log "Removendo imagem com a tag ${IMAGE_TAG:-null} (digest: $IMAGE_DIGEST) no repositorio $REPO_NAME..."
       DELETE_OUTPUT=$(aws ecr batch-delete-image \
         --region $REGION \
         --repository-name $REPO_NAME \
         --image-ids imageDigest=$IMAGE_DIGEST 2>&1)
       
       if [ $? -eq 0 ]; then
-        log "Successfully deleted image with tag ${IMAGE_TAG:-null} in repository $REPO_NAME"
+        log "Imagem com a tag ${IMAGE_TAG:-null} removida com sucesso do repositorio $REPO_NAME"
       else
-        log "Failed to delete image with tag ${IMAGE_TAG:-null} in repository $REPO_NAME"
+        log "Falha ao removar a imagem com a tag ${IMAGE_TAG:-null} no repositorio $REPO_NAME"
         log "Error: $DELETE_OUTPUT"
       fi
     else
-      log "Skipping image with tag ${IMAGE_TAG:-null} in repository $REPO_NAME (either 'latest' or no valid digest)"
+      log "Pulando a imagem com a tag ${IMAGE_TAG:-null} no repositorio $REPO_NAME ('latest' ou inválido)"
     fi
   done
 
-  log "Cleanup completed for container repository: $REPO_NAME"
+  log "Limpeza completa para o repositório: $REPO_NAME"
 done
 
-log "ECR cleanup script completed. Logs saved in $LOG_FILE"
+log "Limpeza do ecr completa , logs registrados em : $LOG_FILE"
